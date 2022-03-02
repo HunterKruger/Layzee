8096f3498552479c68eae204ce63bb3bb54a2cc4
Yuan Feng <yuanfeng9623@outlook.com>
2022-03-02 09:59:24 +0800

    add new func for splitting

--- layzee/splitter_sampler.py
+++ layzee/splitter_sampler.py
@@ -1,47 +1,61 @@
 from sklearn.model_selection import train_test_split
 
 
-def split_df(df, test_ratio=0.2, target=None, random_state=1337):
+def split_df(df, test_ratio=0.2, val_ratio=None, target=None, random_state=1337):
     """
     Split a dataset into training set and test set
     df -> (train, test)
        -> (X_train, X_test, y_train, y_test)
     :param df: a DataFrame to be split
     :param test_ratio: ratio of test set, 0-1
+    :param val_ratio: ratio of validation set, 0-1
+        split into (train, test) if not specified
+        split into (train, val, test) if specified
     :param target:
         split into (train, test) if not specified
         split into (X_train, X_test, y_train, y_test) if specified
     :param random_state: random state
     """
     if target:
-        X = df.drop(target, axis=1, inplace=False)
-        y = df[target]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=random_state)
-        return X_train, X_test, y_train, y_test
+        if val_ratio:
+            count = df.shape[0]
+            val_count = int(count * val_ratio)
+            test_count = int(count * test_ratio)
+            df = df.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            val = df[:val_count]
+            test = df[val_count:(val_count + test_count)]
+            train = df[(val_count + test_count):]
+            val = val.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            test = test.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            train = train.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            X_train = train.drop(target, axis=1, inplace=False)
+            X_val = val.drop(target, axis=1, inplace=False)
+            X_test = test.drop(target, axis=1, inplace=False)
+            y_train = train[target]
+            y_val = val[target]
+            y_test = test[target]
+            return X_train, X_val, X_test, y_train, y_val, y_test
+        else:
+            X = df.drop(target, axis=1, inplace=False)
+            y = df[target]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=random_state)
+            return X_train, X_test, y_train, y_test
     else:
-        train, test = train_test_split(df, test_size=test_ratio, random_state=random_state)
-        return train, test
-
-
-def split_Xy(X, y, test_ratio=0.2, random_state=1337):
-    """
-    (X,y) -> (X_train, X_test, y_train, y_test)
-    :param X: features, 2d
-    :param y: target, 1d
-    :param test_ratio: ratio of test set, 0-1
-    :param random_state: random state
-    """
-    return train_test_split(X, y, test_size=test_ratio, random_state=random_state)
-
-
-def split_train_test(train, test, target):
-    """
-    (train,test) -> (X_train, X_test, y_train, y_test)
-    :param target: target column
-    :param train: training set with features and target
-    :param test: test set with features and target
-    """
-    return train.drop(target, axis=1), test.drop(target, axis=1), train[target], test[target]
+        if val_ratio:
+            count = df.shape[0]
+            val_count = int(count * val_ratio)
+            test_count = int(count * test_ratio)
+            df = df.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            val = df[:val_count]
+            test = df[val_count:(val_count + test_count)]
+            train = df[(val_count + test_count):]
+            val = val.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            test = test.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            train = train.sample(frac=1, random_state=random_state).reset_index(drop=True)
+            return train, val, test
+        else:
+            train, test = train_test_split(df, test_size=test_ratio, random_state=random_state)
+            return train, test
 
 
 def sampler(df, n, col=None, random_state=1337):
